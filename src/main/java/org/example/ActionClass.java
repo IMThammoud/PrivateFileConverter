1b05db51ef22e7a051a736bea1f6c9d58d1f69ac
hassan <trondl-github@proton.me>
2025-09-17 20:21:59 +0200

    prepared the conversion method for img-to-pdf

--- src/main/java/org/example/ActionClass.java
+++ src/main/java/org/example/ActionClass.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 
 public class ActionClass {
     public ActionClass(){}
@@ -11,9 +12,16 @@ public class ActionClass {
     public void imgToPDFButtonClicked(){
         JFileChooser jFileChooser = new JFileChooser();
         FileNameExtensionFilter fileNameExtensionFilter = new FileNameExtensionFilter("Filter for PNG and JPG", "png", "jpg", "jpeg");
+        jFileChooser.setFileFilter(fileNameExtensionFilter);
         int val = jFileChooser.showOpenDialog(null);
         if (val == JFileChooser.APPROVE_OPTION) {
             System.out.println("File chosen: " + jFileChooser.getSelectedFile().getName());
+
+            // Conversion process
         }
+    }
+
+    private void convertImgToPdf(File file){
+        
     }
 }
