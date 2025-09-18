5d6af159f0dceaa79a85e263e583852df5b72ede
hassan <trondl-github@proton.me>
2025-09-18 11:33:26 +0200

    conversion from img to pdf works now but layout should be automatically set by comparing width and height of img so the doc knows if landscape or vertical pdf should be created.

--- src/main/java/org/example/ActionClass.java
+++ src/main/java/org/example/ActionClass.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 public class ActionClass {
     public ActionClass(){}
@@ -16,12 +25,39 @@ public class ActionClass {
         int val = jFileChooser.showOpenDialog(null);
         if (val == JFileChooser.APPROVE_OPTION) {
             System.out.println("File chosen: " + jFileChooser.getSelectedFile().getName());
+            System.out.println("File Path: " + jFileChooser.getSelectedFile().getPath());
+            System.out.println("HomeDir of user: " + System.getProperty("user.home"));
 
             // Conversion process
+            try {
+                convertImgToPdf(jFileChooser.getSelectedFile());
+            } catch (IOException | DocumentException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    private void convertImgToPdf(File file){
-        
+    private void convertImgToPdf(File file) throws IOException, DocumentException {
+        FileOutputStream outputStream = new FileOutputStream(System.getProperty("user.home") + "/img_to_PDF" + UUID.randomUUID() + ".pdf");
+        Document document = new Document();
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, outputStream);
+
+        pdfWriter.open();
+        document.open();
+
+        Image imageToBeConverted = Image.getInstance(file.getPath());
+        System.out.println(imageToBeConverted.getWidth());
+        System.out.println(imageToBeConverted.getHeight());
+
+        // Make the Document horizontal if the Width of the IMG is greater than the height
+        // Rotating doesnt work for some reason?
+        // try another layout format than A4 maybe
+        if (imageToBeConverted.getWidth() > imageToBeConverted.getHeight()) {
+            document.setPageSize(PageSize.A4.rotate());
+        }
+        document.add(Image.getInstance(file.getPath()));
+
+        document.close();
+        pdfWriter.close();
     }
 }
